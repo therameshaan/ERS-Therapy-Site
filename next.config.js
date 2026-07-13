ba15f429b2d0b5aeb3bdf9e36ca22d91d4439903
Eshaan Segavalu <eshaansegavalu@savithas-mbp.lan>
2026-07-13 00:18:20 -0500

    chore: migrate images.domains to remotePatterns and remove deprecated swcMinify

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
   },
 }
 
